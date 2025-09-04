cd1747b0eca209d3c75f833eb78d2b168ee25426
Kir1llka <108978913+Kir1llka@users.noreply.github.com>
2025-09-04 18:55:17 +0300

    add Swagger

--- src/main/java/ru/bush/bush_cinema/api/controller/BushCinemaRestController.java
+++ src/main/java/ru/bush/bush_cinema/api/controller/BushCinemaRestController.java
@@ -1,5 +1,11 @@
 package ru.bush.bush_cinema.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.bush.bush_cinema.api.session.ReserveSitRequest;
 import ru.bush.bush_cinema.api.Result;
 import ru.bush.bush_cinema.api.movie.MovieData;
+import ru.bush.bush_cinema.api.session.ReserveSitRequest;
 import ru.bush.bush_cinema.api.session.SessionFullData;
 import ru.bush.bush_cinema.model.Convector;
 import ru.bush.bush_cinema.service.Cart;
@@ -22,12 +28,22 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/cinema")
+@Tag(name = "cinema", description = "Операции с пользовательскими апи")
 public class BushCinemaRestController {
 
     private MovieService movieService;
     private SessionService sessionService;
     private Cart cart;
 
+    @Operation(
+            summary = "Получить все фильмы",
+            description = "Возвращает список всех фильмов",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Успех"),
+                    @ApiResponse(responseCode = "500", description = "Что-то пошло не так",
+                            content = @Content(schema = @Schema(implementation = Result.class)))
+            }
+    )
     @GetMapping("/movies")
     public Result<List<MovieData>> getMovies() {
         var data = movieService.getAllMovies().stream()
@@ -36,12 +52,38 @@ public class BushCinemaRestController {
         return Result.ok(data);
     }
 
+    @Operation(
+            summary = "Получить сеанс по ID",
+            description = "Возвращает информацию о сеансе",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Успешный ответ"),
+                    @ApiResponse(responseCode = "404", description = "Сеанс не найден",
+                            content = @Content(schema = @Schema(implementation = Result.class))),
+                    @ApiResponse(responseCode = "500", description = "Что-то пошло не так",
+                            content = @Content(schema = @Schema(implementation = Result.class)))
+            }
+    )
     @GetMapping("/session/{id}")
-    public Result<SessionFullData> getSession(@PathVariable String id) {
+    public Result<SessionFullData> getSession(
+            @PathVariable
+            @Parameter(name = "id", description = "ID сеанса", example = "дюна-10:30")
+            String id
+    ) {
         var data = Convector.toSessionFullData(sessionService.getSession(id), cart.getSits());
         return Result.ok(data);
     }
 
+    @Operation(
+            summary = "Резервирование места по его данным",
+            description = "Возвращает статус операции",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Успешный ответ"),
+                    @ApiResponse(responseCode = "404", description = "Место не найден",
+                            content = @Content(schema = @Schema(implementation = Result.class))),
+                    @ApiResponse(responseCode = "500", description = "Что-то пошло не так",
+                            content = @Content(schema = @Schema(implementation = Result.class)))
+            }
+    )
     @PostMapping("/sit/reserve")
     public Result<Void> reserveSit(@Valid @RequestBody ReserveSitRequest request) {
         sessionService.reserveSit(request.getSessionId(), request.getCol(), request.getRow());
--- src/main/java/ru/bush/bush_cinema/api/session/SessionFullData.java
+++ src/main/java/ru/bush/bush_cinema/api/session/SessionFullData.java
@@ -1,6 +1,7 @@
 package ru.bush.bush_cinema.api.session;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +10,14 @@ import java.util.List;
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Schema(description = "Полная информация о сеансе")
 public class SessionFullData {
+
+    @Schema(description = "ID сеанса", example = "дюна-10:30")
     private String id;
+
+    @Schema(description = "Время сеанса", example = "10:30")
     private String time;
+
     private List<SitData> sits;
 }
