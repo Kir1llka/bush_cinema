56bf135cfc0b4d94bbafd098d535b4849b27a38f
Zoos <kirillyasyukevich95@gmail.com>
2025-09-04 18:53:14 +0300

    add Swagger

--- src/main/java/ru/bush/bush_cinema/api/movie/MovieData.java
+++ src/main/java/ru/bush/bush_cinema/api/movie/MovieData.java
@@ -1,6 +1,7 @@
 package ru.bush.bush_cinema.api.movie;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,12 +10,26 @@ import java.util.List;
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Schema(description = "Информация о фильме")
 public class MovieData {
+
+    @Schema(description = "ID фильма", example = "3")
     private int id;
+
+    @Schema(description = "Название", example = "Дюна")
     private String name;
+
+    @Schema(description = "Длительность", example = "136")
     private int duration;
+
+    @Schema(description = "Жанры", example = "Фантастика, Драма")
     private String genre;
+
+    @Schema(description = "Год выпуска", example = "2025")
     private int year;
+
+    @Schema(description = "Ссылка на картинку", example = "SUCCESS")
     private String imageLink;
+
     private List<SessionData> sessions;
 }
--- src/main/java/ru/bush/bush_cinema/api/session/SitData.java
+++ src/main/java/ru/bush/bush_cinema/api/session/SitData.java
@@ -1,6 +1,7 @@
 package ru.bush.bush_cinema.api.session;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import ru.bush.bush_cinema.model.session.SitState;
@@ -8,8 +9,15 @@ import ru.bush.bush_cinema.model.session.SitState;
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Schema(description = "Данные о месте на сеансе")
 public class SitData {
+
+    @Schema(description = "Ряд", example = "1")
     private int col;
+
+    @Schema(description = "Место", example = "2")
     private int row;
+
+    @Schema(description = "Состояние места", example = "FREE")
     private SitState state;
 }
