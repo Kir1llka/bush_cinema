737fdb22301b9d017fbe6cf80378f852024234ba
Zoos <kirillyasyukevich95@gmail.com>
2025-09-16 12:01:24 +0300

    fix sits[][] in GET /session/{id}

--- src/main/java/ru/bush/bush_cinema/repository/entities/SessionEntity.java
+++ src/main/java/ru/bush/bush_cinema/repository/entities/SessionEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -27,6 +28,10 @@ public class SessionEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String time;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private MovieEntity movie;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "session_id")
